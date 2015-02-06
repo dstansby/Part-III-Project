6eb417db4c12a4a854a874763519fd676d273f49
dstansby <dstansby@gmail.com>
2015-02-06 11:32:30 +0000

    Fastforward

---
+++ Matlab/Differences.m
@@ -0,0 +1,40 @@
+clear
+
+file = fopen('both_differences.txt');
+bothdata = fscanf(file,' %f %f', [2 inf]);
+fclose(file);
+
+file = fopen('pkikp_differences.txt');
+idata = fscanf(file,' %f %f', [2 inf]);
+fclose(file);
+
+file = fopen('KIKP_differences.txt');
+Idata = fscanf(file,' %f %f', [2 inf]);
+fclose(file);
+
+bothdata = bothdata';
+idata = idata';
+Idata = Idata';
+
+Idata = Idata((Idata(:,1) < 128),:);
+
+errboth = 0.01*ones([size(bothdata,1) 1]);
+erri = 0.01*ones([size(idata,1) 1]);
+errI = 0.01*ones([size(Idata,1) 1]);
+
+figure;
+hold on;
+herrorbar(bothdata(:,2),bothdata(:,1),errboth,'+');
+herrorbar(idata(:,2),idata(:,1),erri,'+');
+herrorbar(Idata(:,2),Idata(:,1),errI,'+');
+
+leg = legend('Combined', 'PKIKP', 'PKiKP');
+leg.FontSize = 12;
+ax = gca;
+ax.YDir = 'reverse';
+ax.XAxisLocation = 'top';
+ax.XLim = [0.6 0.8];
+ax.YLim = [120 130];
+grid on;
+xlabel('Peak to peak difference /s')
+ylabel('Epicentral distance /degrees')
