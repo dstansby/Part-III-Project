d3202e58c7f92541591fac8cb63d2f238fc602ba
dstansby <dstansby@gmail.com>
2015-02-09 21:51:30 +0000

    Added error bars to synthetic differences plot

--- Matlab/min_depth.m
+++ Matlab/min_depth.m
@@ -16,11 +16,11 @@ bothdata = bothdata';
 idata = idata';
 Idata = Idata';
 
-Idata = Idata((Idata(:,1) < 128),:);
+%Idata = Idata((Idata(:,1) < 128),:);
 
-% errboth = 0.01*ones([size(bothdata,1) 1]);
-% erri = 0.01*ones([size(idata,1) 1]);
-% errI = 0.01*ones([size(Idata,1) 1]);
+errboth = 0.01*ones([size(bothdata,1) 1]);
+erri = 0.01*ones([size(idata,1) 1]);
+errI = 0.01*ones([size(Idata,1) 1]);
 % 
 % % Colours
 % bcol = [0 1 1];
@@ -29,20 +29,22 @@ Idata = Idata((Idata(:,1) < 128),:);
 
 figure;
 hold on;
-bhan = scatter(bothdata(:,2),bothdata(:,1),'+');
-ihan = scatter(idata(:,2),idata(:,1),'+');
-Ihan = scatter(Idata(:,2),Idata(:,1),'+');
+scatter(bothdata(:,2),bothdata(:,1),'+');
+scatter(idata(:,2),idata(:,1),'+');
+scatter(Idata(:,2),Idata(:,1),'+');
+ax = gca;
 
-% herrorbar(bothdata(:,2),bothdata(:,1),errboth,bcol);
-% herrorbar(idata(:,2),idata(:,1),erri,icol);
+herrorbar(bothdata(:,2),bothdata(:,1),errboth,ax.ColorOrder(1,:));
+herrorbar(idata(:,2),idata(:,1),erri,ax.ColorOrder(2,:));
+herrorbar(Idata(:,2),Idata(:,1),errI,ax.ColorOrder(3,:));
 
 leg = legend('Combined', 'PKIKP', 'PKiKP');
 leg.FontSize = 12;
-ax = gca;
 ax.YDir = 'reverse';
 ax.XAxisLocation = 'top';
-ax.XLim = [0.6 0.8];
-ax.YLim = [120 130];
+% ax.XLim = [0.6 0.8];
+% ax.YLim = [126 135];
+ax.FontSize = 14;
 grid on;
 xlabel('Peak to peak difference /s')
 ylabel('Epicentral distance /degrees')
