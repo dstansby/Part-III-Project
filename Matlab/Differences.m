94d2736dfa3cd469c99947f3db011259230766c2
dstansby <dstansby@gmail.com>
2015-02-09 19:23:35 +0000

    Calculate and plot residuals

--- Matlab/Differences.m
+++ Matlab/Differences.m
@@ -14,8 +14,33 @@ real = real';
 realErr = 0.02*ones(size(real(:,1)));
 synthErr = 0.02*ones(size(synth(:,1)));
 
-%synth = synth((synth(:,1) > 126),:);
+distances = intersect(synth(:,1),real(:,1));
+diff = zeros([size(distances,1) 2]);
+differr = 0.04*ones([size(diff,1) 1]);
 
+for i = 1:size(distances)
+	d = distances(i);
+	ri = find(~(real(:,1) - d));
+	si = find(~(synth(:,1) - d));
+	si = si(1);
+	
+	dt = real(ri,2) - synth(si,2);
+	diff(i,:) = [d dt];
+end
+
+figure;
+scatter(diff(:,2),diff(:,1),'+');
+ax = gca;
+
+herrorbar(diff(:,2),diff(:,1),differr,ax.ColorOrder(1,:))
+vline(0);
+
+ax.YDir = 'reverse';
+ax.XAxisLocation = 'top';
+xlabel('\delta t /s');
+ylabel('Epicentral distance /^{\circ}');
+
+figure;
 hold on;
 scatter(synth(:,2),synth(:,1),'+');
 herrorbar(synth(:,2),synth(:,1),synthErr,'b');
