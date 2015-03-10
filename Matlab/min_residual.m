e642b131acff72ccd60c35cd64717a213f52b9cb
dstansby <dstansby@gmail.com>
2015-03-10 11:56:18 +0000

    Set global t0

--- Matlab/min_residual.m
+++ Matlab/min_residual.m
@@ -2,13 +2,17 @@ clear
 folder = 'celebessea';
 addpath('Library');
 
+t0 = 0.68;
+
 %% Import data
 data = readfile(['data/' folder '/both_differences.txt'],'%*s %f %f',2);
-data(:,2) = 0.68 - data(:,2);
+data(:,2) = t0 - data(:,2);
+
 %% Find line of best fit
 p = polyfit(data(:,2),data(:,1),4);
 line(:,1) = -1.2:0.1:0.4;
 line(:,2) = polyval(p,line(:,1));
+
 %% Plot data
 figure;
 hold on;
