257bb4020fb41c872623dfbd33ea73fae506ae9c
dstansby <dstansby@gmail.com>
2015-03-25 11:19:58 +0000

    Allow generically sized input

--- Matlab/Library/movingaverage.m
+++ Matlab/Library/movingaverage.m
@@ -1,22 +1,21 @@
 function output = movingaverage(input, n)
 % MOVINGAVERAGE Create moving average of a data series
-%	out = movingaverage(in, n) takes m by 2 matrix 'in' and integer 'n'
+%	out = movingaverage(in, n) takes m by o matrix 'in' and integer 'n'
 %	and outputs the moving average over 'n' points
 %
+%	Data is sorted along the first column of 'in'
+%
 %	David Stansby 2015
-
-if size(input,2) ~= 2
-	error('Please provide a m by 2 matrix as input')
-end
 
 m = n-1;	% 0 if we are taking a 1 point moving average (ie. doing nothing)
 
 input = sortrows(input);
 output = zeros(size(input,1)-m, size(input,2));
 
-for i = 1:(size(xin,1)-m)
+for i = 1:(size(input,1)-m)
 	for j = 0:m
 		output(i,:) = output(i,:) + input(i+j,:);
 	end
 end
+
 output = output./n;
