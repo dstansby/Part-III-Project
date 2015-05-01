2c698530eef290b5ef04ce1e39c25b589c8e365f
dstansby <dstansby@gmail.com>
2015-05-01 11:42:03 +0100

    Plot WH data

--- Matlab/plotVelModelsResiduals.m
+++ Matlab/plotVelModelsResiduals.m
@@ -5,21 +5,29 @@ folder = ('bandasea');
 %% Import data
 realp2p = readfile(['data/' folder '/real_differences.txt'],'%*s %f %f',2);
 ehp2p = readfile(['data/' folder '/eh_residuals.txt'],'%*s %f %f',2);
+whp2p = readfile(['data/' folder '/wh_residuals.txt'],'%*s %f %f',2);
 ak135p2p = readfile(['data/' folder '/PKiKP_differences.txt'],'%*s %f %f',2);
 ak135stationDetails = readfile(['data/' folder '/stationdetails.txt'], '%f %*s %f %f %f %f %f %f %f %f %f %f %f %f %f %f',15);
 
+depths = ak135stationDetails(:,13) - ICBdepth;
+times = ak135stationDetails(:,15) - ak135stationDetails(:,14);
+
+%% Calculate residuals
 realResid = realp2p(:,2) - ak135p2p(:,2);
 ehResid = ehp2p(:,2) - ak135p2p(:,2);
+whResid = whp2p(:,2) - ak135p2p(:,2);
 
-depths = ak135stationDetails(:,13) - ICBdepth;
 
 figure;
 hold on;
 scatter(realResid,depths,'+');
 scatter(ehResid,depths,'+');
+scatter(whResid,depths,'+');
+l = legend('Real', 'EH (11.1 km/s)', 'WH (11.0 km/s)');
 
 ax1 = gca;
 % Plot formatting
+l.Location = 'SouthEast';
 ax1.YDir = 'reverse';
 ax1.XAxisLocation = 'top';
 xlabel('Residual /s');
