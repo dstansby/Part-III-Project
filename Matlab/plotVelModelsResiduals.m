b4131f31185e7fcc440278f2a957edeeb8c12eb7
dstansby <dstansby@gmail.com>
2015-05-01 12:25:12 +0100

    Plot measured velocity

--- Matlab/plotVelModelsResiduals.m
+++ Matlab/plotVelModelsResiduals.m
@@ -1,6 +1,7 @@
 addpath('Library');
 load('ICBdepth');
 folder = ('bandasea');
+ak135vel = 11.0427;
 
 %% Import data
 realp2p = readfile(['data/' folder '/real_differences.txt'],'%*s %f %f',2);
@@ -17,9 +18,9 @@ realResid = realp2p(:,2) - ak135p2p(:,2);
 ehResid = ehp2p(:,2) - ak135p2p(:,2);
 whResid = whp2p(:,2) - ak135p2p(:,2);
 
-realVelChange = realResid./(times - realResid);
-ehVelChange = ehResid./(times - ehResid);
-whVelChange = whResid./(times - whResid);
+realVelChange = ak135vel*((realResid./(times - realResid)) + 1);
+ehVelChange = ak135vel*((ehResid./(times - ehResid)) + 1);
+whVelChange = ak135vel*((whResid./(times - whResid)) + 1);
 
 %% Plot residuals
 figure;
@@ -52,10 +53,12 @@ ax = gca;
 % Plot formatting
 l.Location = 'SouthEast';
 ax.YDir = 'reverse';
-ax.YLim = [2 16];
+ax.YLim = [1 16];
 ax.XAxisLocation = 'top';
-xlabel('Residual /s');
+xlabel('Measured velocity / km/s');
 ylabel('Depth below ICB /km');
 ax.FontSize = 14;
 title(folder);
-vline(0);
+vline(ak135vel);
+vline(11);
+vline(11.1);
